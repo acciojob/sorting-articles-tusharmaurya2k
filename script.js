404ff1d4c8e470412d5750920207a7e0c6ae889b
tusharmaurya2k <89917676+tusharmaurya2k@users.noreply.github.com>
2026-03-06 01:48:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+const bands = [
+'The Plot in You',
+'The Devil Wears Prada',
+'Pierce the Veil',
+'Norma Jean',
+'The Bled',
+'Say Anything',
+'The Midway State',
+'We Came as Romans',
+'Counterparts',
+'Oh, Sleeper',
+'A Skylit Drive',
+'Anywhere But Here',
+'An Old Dog'
+];
+
+function strip(name){
+  return name.replace(/^(a |an |the )/i, '').trim();
+}
+
+const sortedBands = bands.sort((a,b)=>{
+  return strip(a).localeCompare(strip(b));
+});
+
+const ul = document.getElementById("band");
+
+ul.innerHTML = sortedBands
+  .map(band => `<li>${band}</li>`)
+  .join('');
